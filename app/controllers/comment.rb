2f10a5e92d2c23edc8692c624a7a17c9bc6463a2
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-01 15:32:14 -0400

    Finish comment pages

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,13 +1,41 @@
+# Get Routes
 get '/joke/:joke_id/comments/:id/edit' do
-  @comment =Comment.find_by(joke_id: params[:joke_id], id: params[:id])
+  @comment = Comment.find_by(joke_id: params[:joke_id], id: params[:id])
   erb :'comments/edit'
 end
 
+get '/joke/:joke_id/comments/:id/delete' do
+  @comment =Comment.find_by(joke_id: params[:joke_id], id: params[:id])
+  erb :'comments/delete'
+end
+
+# Update Routes
 post '/comment' do
   comment = Comment.new(message: params[:message], user_id: current_user.id, joke_id: params[:joke_id])
   if comment.save
     redirect "/joke/#{params[:joke_id]}"
   else
     [404, "Where you tryna go?"]
+  end
+end
+
+put '/joke/:joke_id/comments/:id/edit' do
+  comment = Comment.find_by(joke_id: params[:joke_id], id: params[:id])
+  if comment
+    comment.update_attributes(message: params[:message])
+    comment.save
+    redirect "/joke/#{comment.joke_id}"
+  else
+    [404, "WHERE ARE THE EDITS?"]
+  end
+end
+
+delete '/joke/:joke_id/comments/:id/delete' do
+  comment = Comment.find_by(joke_id: params[:joke_id], id: params[:id])
+  if comment
+    comment.destroy
+    redirect "/joke/#{comment.joke_id}"
+  else
+    [404, "THE COMMENTS NOT HERE! HEHEHEHHEEhhehehe"]
   end
 end
