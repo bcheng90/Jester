d4def4902ec3d03ef591d5e610736aa9b6dfd27f
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-01 15:37:38 -0400

    Able to get new comments onto page with ajax

--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,15 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $('form#add_comment').on('submit', function(event){
+    event.preventDefault();
+    $.ajax({url: '/comment',
+           method: 'post',
+           data: $(event.target).serialize(),
+           dataType: 'html'
+    }).done(function(data){
+      $('#add_comment').reset();
+    });
+  });
 });
