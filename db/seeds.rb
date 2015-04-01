547eedd7794280faa45af6c880eb022910622020
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-01 09:40:11 -0400

    Add joke_id to all comments.EVERYTHING IS RELATING

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@ users.each do |user|
     Joke.create(line1: Faker::Hacker.say_something_smart, user_id: user.id, created_at: "#{rand(2010..2014)}-#{rand(1..12)}-#{rand(1..28)} 03:14:15")
   end
   10.times do
-    Comment.create(message: Faker::Hacker.say_something_smart, user_id: user.id, joke_id: nil, created_at: "#{rand(2010..2014)}-#{rand(1..12)}-#{rand(1..28)} 03:14:15")
+    Comment.create(message: Faker::Hacker.say_something_smart, user_id: user.id, joke_id: rand(1..90), created_at: "#{rand(2010..2014)}-#{rand(1..12)}-#{rand(1..28)} 03:14:15")
   end
 end
