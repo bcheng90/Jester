a770b8e7c99b25ad921e8bd527344d552cb1e8f7
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-01 14:51:04 -0400

    Begin edit and delete of comments. Fix background image from moving everywhere

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,3 +1,8 @@
+get '/joke/:joke_id/comments/:id/edit' do
+  @comment =Comment.find_by(joke_id: params[:joke_id], id: params[:id])
+  erb :'comments/edit'
+end
+
 post '/comment' do
   comment = Comment.new(message: params[:message], user_id: current_user.id, joke_id: params[:joke_id])
   if comment.save
@@ -6,5 +11,3 @@ post '/comment' do
     [404, "Where you tryna go?"]
   end
 end
-
-gi
