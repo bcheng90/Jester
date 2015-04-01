e9944c980f92618c2f50f726191d8244c1549963
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-01 13:21:57 -0400

    Joke page is partially complete

--- app/controllers/joke.rb
+++ app/controllers/joke.rb
@@ -1,8 +1,49 @@
+# Get Routes
+
 get '/joke/new' do
-  erb :'joke/new'
+  if current_user
+    erb :'jokes/new'
+  else
+    redirect '/login'
+  end
+end
+
+get '/joke/:id/edit' do
+  @joke = Joke.find_by(id: params[:id])
+  erb :'jokes/edit'
+end
+
+get '/joke/:id/delete' do
+  @joke = Joke.find_by(id: params[:id])
+  erb :'jokes/delete'
 end
 
 get '/joke/:id' do
   @joke = Joke.find_by(id: params[:id])
-  erb :'joke/show'
+  erb :'jokes/show'
+end
+
+# Update Routes
+
+post '/joke/new' do
+  new_joke = Joke.new(line1: params[:line1], line2: params[:line2], user_id: current_user.id)
+  if new_joke.save
+    redirect "/joke/#{new_joke.id}"
+  else
+    [404, "WHY SO SERIOUS?"]
+  end
+end
+
+put '/joke/:id' do
+  joke = Joke.find_by(id: params[:id])
+  joke.update_attributes(line1: params[:line1],
+                         line2: params[:line2],
+                         user_id: current_user.id)
+  redirect "/joke/#{joke.id}"
+end
+
+delete '/joke/:id' do
+  joke = Joke.find_by(id: params[:id])
+  joke.destroy
+  redirect '/'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,8 @@ users.each do |user|
     Comment.create(message: Faker::Hacker.say_something_smart, user_id: user.id, joke_id: rand(1..100), created_at: "#{rand(2010..2014)}-#{rand(1..12)}-#{rand(1..28)} 03:14:15")
   end
 end
+
+Joke.create(line1: "How many 'Hoa's does it take to switch a lightbulb?", line2: "Hoane.", user_id: 18)
+Joke.create(line1: "How many DBC students does it take to switch a lightbulb?", line2: "Two students pairing, one coach helping, and finger snaps ready when it's installed.", user_id: 16)
+Joke.create(line1: "What did Hoa say when everyone was trying to talk to him with his headphones in?", line2: "Hoa-t?",user_id: 9)
+Joke.create(line1: "Why am I making jokes during my assessment/solo challenge?", line2: "Because I'm probably not gonna be listening to the joke during annoucements. Coding brain.", user_id: 1)
